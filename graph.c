2337547f260164e7fde4f7bc4dc14a7e1b869b56
Connor Abbott <cwabbott0@gmail.com>
2012-07-25 12:15:53 -0400

    add stuff

---
+++ graph.c
@@ -0,0 +1,15 @@
+#include "graph.h"
+
+
+void floyd_warshall(distance_matrix g) {
+	for (int k = 0; k < g.n; k++) {
+		for (int i = 0; i < g.n; i++) {
+			for (int j = 0; j < g.n; j++) {
+				int dist = g.distances[g.n*i + k] + g.distances[g.n*k + j];
+				if(dist < g.distances[g.n*i + j]) {
+					g.distances[g.n*i + j] = dist;
+				}
+			}
+		}
+	}
+}
---
+++ graph.h
@@ -0,0 +1,18 @@
+#ifndef GRAPH_H
+
+
+//Represents when there is no connection
+//graph.c adds stuff to infinity (crazy, I know),
+//So make sure it won't overflow
+#define GRAPH_INFINITY ((int)1000000)
+
+typedef struct {
+	int *distances; //INFINITY for no connection
+	int n; //number of vertices
+} distance_matrix;
+
+void floyd_warshall(distance_matrix g);
+void extend_dist_matrix(distance_matrix dest, distance_matrix src);
+
+#define GRAPH_H
+#endif //GRAPH_H
