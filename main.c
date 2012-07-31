5cd8ad55c1101a6e5c14690a3ac9f4aaa58e3d91
Connor Abbott <cwabbott0@gmail.com>
2012-07-31 14:29:34 -0400

    add some geng stuff

--- main.c
+++ main.c
@@ -2,9 +2,30 @@
 #include "hash_set.h"
 #include <stdio.h>
 
+int geng(int argc, char *argv[]); //entry point for geng
+
+//Wrapper around the geng entry function
+//n is the number of vertices
+int call_geng(unsigned n, unsigned k)
+{
+	char n_buf[10], k_buf[10];
+	char *geng_args[] = {
+		"geng",
+		"-ucq",
+		"",
+		""
+	};
+	sprintf(k_buf, "-D%d", k);
+	sprintf(n_buf, "%d", n);
+	geng_args[2] = k_buf;
+	geng_args[3] = n_buf;
+	return geng(4, geng_args);
+}
+
 int main(void)
 {
-	
+	call_geng(10, 4);
+	printf("%u\n", num_graphs);
 	return 0;
 }
 
